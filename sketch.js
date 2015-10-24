0b49a8b68901573d8546b9c2190a84478d4dce55
cybermarinella <marinella.mastrosimone@gmail.com>
2015-10-25 00:10:29 +0200

    funzionante

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /// funzionante
 
@@ -9,8 +8,9 @@ var cardellinocanto;
 
 var hidden = true;
 var counterStart = false;
+var on = true;
 
-function preload() {  // preload() runs once
+function preload() {
     cardellinocanto = loadSound('assets/CantoCardellinoSiciliano.mp3');
     gabbia = loadImage("assets/gabbia.png");
     cardellino = loadImage("assets/cardellino.png");
@@ -24,91 +24,66 @@ function setup() {
   noStroke();
   ellipseMode(CENTER);
   rectMode(CENTER); 
-
   mousePressed();
   cardellinocanto.play();
+  frameRate(5);
 }
 
 function draw() {
-
-  // if(counterStart) {
-  //  if(frameCount % 5 == 0) {
-  //   hidden = !hidden;
-  //   console.log('--', frameCount);
-  //   drawBird();
-  //   counterStart = false;
-  //  }
-  // }
-
-
+  //console.log('--', frameCount);
 }
 
 var t;
 
 function mousePressed() {
-  //fai partire il counter
-  counterStart = true;
-   hidden = !hidden;
-    console.log('timer tick');
-    drawBird();
-
-//dag
-  clearInterval(t); // be safe
-  t = setInterval( function(){ 
-    hidden = !hidden;
-    console.log('timer tick');
-    drawBird();    
-  }, 500);
+  cardellinocanto.stop();
 }
 
 function keyPressed() {
-  console.log(key);
-  if(key==='K') {
-
-  }
-  clearInterval(t);
+  if(key==="A"){
+    on=true;
+    cardellinocanto.stop();
+    hidden = !hidden;
+    console.log(key);
+    //drawGabbia();
+    clearInterval(t); // be safe
+    t = setInterval( function(){ 
+      //console.log('timer tick' , hidden);
+      hidden = !hidden;
+      drawGabbia();    
+    }, 5);
+  }  else if(key==='K') {
+    on=false;
+    cardellinocanto.play();
+    console.log(key);
+    clearInterval(t);
+    hidden = true;
+  };
 }
 
-function drawBird() {
-  if ( hidden ) { // .isPlaying() returns a boolean
-    cardellinocanto.stop();
-    background(0);
-    fill(255)
-    ellipse(width*0.5, height*0.5, height/1.618, height/1.618);
-    image(gabbia, width/2-400/2, height/2-450/2-10, 400, 450);
-  } else {
-    background(244);
+function drawGabbia() {
+  if(on==true){
+    if ( hidden ) {
+      background(0);
+      image(gabbia, width/2-800/2-50, height/2-700/2-20, 880, 730);
+    } else {
+      background(244);
+      fill(255)
+      ellipse(width*0.5, height*0.5, height/1.618, height/1.618);
+      image(cardellino, width/2-260/2-40, height/2-400/2+60, 260, 400);
+    }
+  }else{
     fill(255)
     ellipse(width*0.5, height*0.5, height/1.618, height/1.618);
     image(cardellino, width/2-260/2-40, height/2-400/2+60, 260, 400);
-    cardellinocanto.play();
   }
 }
-=======
-var num = 8;
-var diametro;
 
-function setup() {
-  createCanvas(1366, 768);
-  diametro = height/1.618;
-  ellipseMode(CENTER);
-}
-
-function draw() {
-  background(244);
-  stroke(200);
-  noFill();
-  ellipse(width/2, height/2, diametro , diametro);
-
-  for(var i=0; i < num; ++i ){
-    noStroke();
-    fill(255, 0, 0, 10*i);
-    var x =width/2  +i*5;
-    var y =height/2  +i*5;
-    ellipse(x, y, diametro , diametro);
-
-    //narure of code
-  }
-}
- 
->>>>>>> 68613cc5facbdaf8a20178960f30dfd38576caa8
+  // if(counterStart) {
+  //  if(frameCount % 5 == 0) {
+  //   hidden = !hidden;
+  //   console.log('--', frameCount);
+  //   drawGabbia();
+  //   counterStart = false;
+  //  }
+  // }
