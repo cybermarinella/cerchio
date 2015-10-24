740dda151d8e18abd08d640b524f1d55a12a4c4d
cybermarinella <marinella.mastrosimone@gmail.com>
2015-10-25 00:15:48 +0200

    funzionante meglio

--- sketch.js
+++ sketch.js
@@ -1,4 +1,82 @@
 
+var Polygon = function() {
+
+  var sides             = floor(random(5, 18));
+  var cx                = random(0, width);
+  var cy                = random(0, height);
+  var angle             = random(0, 360);
+  var radius            = 0;
+  var color             = colors[floor(random(0, colors.length))];
+  var lineWeight        = random(0.75, 1.5);
+  var radiusIncrement   = random(4, 9);
+  var rotationIncrement = random(-2, 2);
+  var points            = [];
+
+  var updatePoints = function() {
+    points = [];
+    for (i = 0; i < sides; i++) {
+      var a = angle + (i / sides) * 360;
+      var x = cx + (cos(radians(a)) * radius);
+      var y = cy + (sin(radians(a)) * radius);
+      points.push([x, y]);
+    }
+  }
+
+  var draw = function() {
+    updatePoints();
+    stroke(color);
+    strokeWeight(lineWeight);
+    noFill();
+    beginShape();
+    for (i = 0; i < sides; i++) {
+      vertex(points[i][0], points[i][1]);
+    }
+    endShape(CLOSE);
+    radius = radius + radiusIncrement;
+    angle = angle + rotationIncrement;
+  }
+
+  var getRadius = function() {
+    return radius;
+  }
+
+  return {
+    'draw': draw,
+    'getRadius': getRadius
+  }
+}
+
+
+var p;
+var colors;
+var radiusLimit;
+
+var dim;
+var gabbia;
+var cardellino;
+var cardellinocanto;
+
+function preload() {  // preload() runs once
+    cardellinocanto = loadSound('assets/CantoCardellinoSiciliano.mp3');
+    gabbia = loadImage("assets/gabbia.png");
+    cardellino = loadImage("assets/cardellino.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(244);
+  frameRate(48);
+  radiusLimit = sqrt(pow(windowWidth, 2) + pow(windowHeight, 2));
+  colors = [
+    color(155),
+    color(142),
+    color(155),
+    color(140)
+  ];
+  //noLoop();
+  p = new Polygon();
+}
+
 /// funzionante
 
 var dim;
@@ -37,6 +115,9 @@ var t;
 
 function mousePressed() {
   cardellinocanto.stop();
+  console.log(key);
+  clearInterval(t);
+  hidden = true;
 }
 
 function keyPressed() {
